2f851858bc50c02cb098b63db056ced18c0add69
Sujith <ps335@snu.edu.in>
2024-05-29 21:22:20 +0530

    fix game work

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { game } from '../games';
+import { gamearr } from '../games';
 
 function Games() {
 
@@ -17,29 +17,42 @@ function Games() {
   useEffect(() => {
     // Add CSS to head
     const style = document.createElement('style');
-    style.textContent = game[gameChosen].css;
+    style.textContent = gamearr[gameChosen].css;
     document.head.append(style);
 
     // Add JS to body
     const script = document.createElement('script');
-    script.textContent = game[gameChosen].js;
+    script.textContent = gamearr[gameChosen].js;
     document.body.append(script);
+
+    if(gamearr[gameChosen].res !== undefined) {
+      // load the resourses js  
+      const script2 = document.createElement('script');
+      script2.classList.add('res-js');
+      script2.src = gamearr[gameChosen].res;
+      script2.async = true;
+      document.body.append(script2);
+    }
 
     // Clean up on unmount
     return () => {
       document.head.removeChild(style);
       document.body.removeChild(script);
+      const resjs = document.querySelector('.res-js');
+      if (resjs) {
+        document.body.removeChild(resjs);
+      }
     };
   }, [gameChosen]);
 
   return (
     <div className='game-total-container'>
       <div className='game-list'>
-        {game.map((game, index) => (
-          <button key={index} onClick={() => setGame(index)}>{game.name}</button>
+        {gamearr.map((gamec, index) => (
+          <button key={index} onClick={() => setGame(index)}>{gamec.name}</button>
         ))}
       </div>
-    <div dangerouslySetInnerHTML={{ __html: game[gameChosen].html }} className='game-cont-2'/>
+    <div dangerouslySetInnerHTML={{ __html: gamearr[gameChosen].html }} className='game-cont-2'/>
     </div>
   )
 }
